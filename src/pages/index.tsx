a213689e48a5c18340c68aa65a58e7fe687f610b
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2021-12-14 20:29:33 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,16 @@ import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import Header from '../components/Header';
 import {FiCalendar, FiUser} from 'react-icons/fi'
-
+import Prismic from '@prismicio/client'
 
 import { getPrismicClient } from '../services/prismic';
 
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
+import {format} from 'date-fns'
+import {ptBR} from 'date-fns/locale';
 
 interface Post {
   uid?: string;
@@ -30,44 +33,46 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({postsPagination}: HomeProps): JSX.Element{
+  const formattedPost = postsPagination.results.map(post=>{
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR
+        }
+      )
+    }
+  })
+  
+  const [posts, setPosts] = useState<Post[]>(formattedPost)
   return (
     <>
       <main className={commonStyles.container}>
         <Header/>
         <div className={styles.posts}>
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>Title</strong>
-              <p>Something subtitle uehuhehdeudhe idhehdehien</p>
-              <ul>
-                <li>
-                  <FiCalendar/>
-                  15 Mar 2021                
-                </li>
-                <li>
-                  <FiUser/>
-                  Murilo Souza
-                </li>
-              </ul>
-            </a>
-          </Link>
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>Title</strong>
-              <p>Something subtitle uehuhehdeudhe idhehdehien</p>
-              <ul>
-                <li>
-                  <FiCalendar/>
-                  15 Mar 2021                
-                </li>
-                <li>
-                  <FiUser/>
-                  Murilo Souza
-                </li>
-              </ul>
-            </a>
-          </Link>
+          {
+            posts.map(post =>(
+              <Link href={`/post/${post.uid}`} key={post.uid}>
+                <a className={styles.post}>
+                  <strong>{post.data.title}</strong>
+                  <p>{post.data.subtitle}</p>
+                  <ul>
+                    <li>
+                      <FiCalendar/>
+                      {post.first_publication_date}              
+                    </li>
+                    <li>
+                      <FiUser/>
+                      {post.data.author}
+                    </li>
+                  </ul>
+                </a>
+              </Link>
+            ))
+          }
           <button type='button'>
             Carregar mais posts
           </button>
@@ -77,9 +82,34 @@ export default function Home() {
   )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+ export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+    const postsResponse = await prismic.query([
+      Prismic.Predicates.at('document.type', 'posts')
+    ], {
+      pageSize: 1,
+    });
 
-//   // TODO
-// };
+    const posts = postsResponse.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: post.first_publication_date,
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author
+        }
+      }
+    })
+    console.log(postsResponse.results)
+    const postsPagination = {
+      next_page: postsResponse.next_page,
+      results: posts
+    }
+
+    return {
+      props: {
+        postsPagination
+      }
+    }
+ };
